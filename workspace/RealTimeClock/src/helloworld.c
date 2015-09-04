996cfcc784618e239a9c6bf3a5a91171d6cbc2e1
Jacob White <jacob.white@byu.net>
2015-09-04 09:50:46 -0600

    Modify interrupt handler to print simple debug and status messages.

--- workspace/RealTimeClock/src/helloworld.c
+++ workspace/RealTimeClock/src/helloworld.c
@@ -3,68 +3,102 @@
 #include "platform.h"       // Enables caching and other system stuff.
 #include "mb_interface.h"   // provides the microblaze interrupt enables, etc.
 #include "xintc_l.h"        // Provides handy macros for the interrupt controller.
- 
+
 XGpio gpLED;  // This is a handle for the LED GPIO block.
 XGpio gpPB;   // This is a handle for the push-button GPIO block.
- 
+
+#define PUSH_BUTTONS_CENTER 0x01
+#define PUSH_BUTTONS_RIGHT 0x02
+#define PUSH_BUTTONS_DOWN 0x04
+#define PUSH_BUTTONS_LEFT 0x08
+#define PUSH_BUTTONS_UP 0x10
+
+int buttonTimer = 0;
+
 // This is invoked in response to a timer interrupt.
 // It does 2 things: 1) debounce switches, and 2) advances the time.
-void timer_interrupt_handler() {
+void timer_interrupt_handler()
+{
+	static int i = 0;
+	i++;
+	if(i == 1000)
+	{
+		//XPAR_PUSH_BUTTONS_5BITS_BASEADDR
+		i = 0;
+		print("+ 10 seconds\n\r");
+	}
+	if(buttonTimer > 0)
+		buttonTimer--;
 }
- 
+
 // This is invoked each time there is a change in the button state (result of a push or a bounce).
-void pb_interrupt_handler() {
-  // Clear the GPIO interrupt.
-  XGpio_InterruptGlobalDisable(&gpPB);                // Turn off all PB interrupts for now.
-  int currentButtonState = XGpio_DiscreteRead(&gpPB, 1);  // Get the current state of the buttons.
-  
-  print("do something");
-  
-  // You need to do something here.
-  XGpio_InterruptClear(&gpPB, 0xFFFFFFFF);            // Ack the PB interrupt.
-  XGpio_InterruptGlobalEnable(&gpPB);                 // Re-enable PB interrupts.
+void pb_interrupt_handler()
+{
+	// Clear the GPIO interrupt.
+	XGpio_InterruptGlobalDisable(&gpPB);                // Turn off all PB interrupts for now.
+	int currentButtonState = XGpio_DiscreteRead(&gpPB, 1);  // Get the current state of the buttons.
+
+	if(currentButtonState && buttonTimer == 0)
+	{
+		buttonTimer = 20;
+		xil_printf("do something %d \n\r", currentButtonState);
+	}
+
+	// You need to do something here.
+	XGpio_InterruptClear(&gpPB, 0xFFFFFFFF);            // Ack the PB interrupt.
+	XGpio_InterruptGlobalEnable(&gpPB);                 // Re-enable PB interrupts.
 }
- 
+
 // Main interrupt handler, queries the interrupt controller to see what peripheral
 // fired the interrupt and then dispatches the corresponding interrupt handler.
 // This routine acks the interrupt at the controller level but the peripheral
 // interrupt must be ack'd by the dispatched interrupt handler.
-void interrupt_handler_dispatcher(void* ptr) {
+void interrupt_handler_dispatcher(void* ptr)
+{
 	int intc_status = XIntc_GetIntrStatus(XPAR_INTC_0_BASEADDR);
 	// Check the FIT interrupt first.
-	if (intc_status & XPAR_FIT_TIMER_0_INTERRUPT_MASK){
+	if (intc_status & XPAR_FIT_TIMER_0_INTERRUPT_MASK)
+	{
 		XIntc_AckIntr(XPAR_INTC_0_BASEADDR, XPAR_FIT_TIMER_0_INTERRUPT_MASK);
 		timer_interrupt_handler();
 	}
 	// Check the push buttons.
-	if (intc_status & XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK){
+	if (intc_status & XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK)
+	{
+		XIntc_AckIntr(XPAR_INTC_0_BASEADDR, XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK);
+		pb_interrupt_handler();
+	}
+	// Check the switches
+	if (intc_status & XPAR_DIP_SWITCHES_8BITS_IP2INTC_IRPT_MASK)
+	{
 		XIntc_AckIntr(XPAR_INTC_0_BASEADDR, XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK);
 		pb_interrupt_handler();
 	}
 }
- 
-int main (void) {
-    init_platform();
-    // Initialize the GPIO peripherals.
-    int success;
-    print("hello world\n\r");
-    success = XGpio_Initialize(&gpPB, XPAR_PUSH_BUTTONS_5BITS_DEVICE_ID);
-    // Set the push button peripheral to be inputs.
-    XGpio_SetDataDirection(&gpPB, 1, 0x0000001F);
-    // Enable the global GPIO interrupt for push buttons.
-    XGpio_InterruptGlobalEnable(&gpPB);
-    // Enable all interrupts in the push button peripheral.
-    XGpio_InterruptEnable(&gpPB, 0xFFFFFFFF);
- 
-    microblaze_register_handler(interrupt_handler_dispatcher, NULL);
-    XIntc_EnableIntr(XPAR_INTC_0_BASEADDR,
-    		(XPAR_FIT_TIMER_0_INTERRUPT_MASK | XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK));
-    XIntc_MasterEnable(XPAR_INTC_0_BASEADDR);
-    microblaze_enable_interrupts();
- 
-    while(1);  // Program never ends.
- 
-    cleanup_platform();
- 
-    return 0;
+
+int main (void)
+{
+	init_platform();
+	// Initialize the GPIO peripherals.
+	int success;
+	print("hello world\n\r");
+	success = XGpio_Initialize(&gpPB, XPAR_PUSH_BUTTONS_5BITS_DEVICE_ID);
+	// Set the push button peripheral to be inputs.
+	XGpio_SetDataDirection(&gpPB, 1, 0x0000001F);
+	// Enable the global GPIO interrupt for push buttons.
+	XGpio_InterruptGlobalEnable(&gpPB);
+	// Enable all interrupts in the push button peripheral.
+	XGpio_InterruptEnable(&gpPB, 0xFFFFFFFF);
+
+	microblaze_register_handler(interrupt_handler_dispatcher, NULL);
+	XIntc_EnableIntr(XPAR_INTC_0_BASEADDR,
+			(XPAR_FIT_TIMER_0_INTERRUPT_MASK | XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK));
+	XIntc_MasterEnable(XPAR_INTC_0_BASEADDR);
+	microblaze_enable_interrupts();
+
+	while(1);  // Program never ends.
+
+	cleanup_platform();
+
+	return 0;
 }
