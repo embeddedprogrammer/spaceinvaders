5b4734544d5ab288550d3d010556a0e105ad70ce
Jacob White <jacob.white@byu.net>
2015-09-16 10:24:08 -0600

    Debounce buttons using timer (waits 50 ms after any change before doing something). Display time, increment time, and respond to push buttons using state machine.

--- workspace/RealTimeClock/src/helloworld.c
+++ workspace/RealTimeClock/src/helloworld.c
@@ -3,6 +3,7 @@
 #include "platform.h"       // Enables caching and other system stuff.
 #include "mb_interface.h"   // provides the microblaze interrupt enables, etc.
 #include "xintc_l.h"        // Provides handy macros for the interrupt controller.
+#include <stdbool.h>
 
 XGpio gpLED;  // This is a handle for the LED GPIO block.
 XGpio gpPB;   // This is a handle for the push-button GPIO block.
@@ -13,35 +14,150 @@ XGpio gpPB;   // This is a handle for the push-button GPIO block.
 #define PUSH_BUTTONS_LEFT 0x08
 #define PUSH_BUTTONS_UP 0x10
 
-int buttonTimer = 0;
+
+
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
+
+int debounceTimer = 0;
+bool debounceTimerEnable = false;
+int debouncedButtonState = 0;
+
+void debounceButtons_tick()
+{
+	if (debounceTimerEnable)
+	{
+		debounceTimer++;
+		if (debounceTimer == 5) //50ms
+		{
+			debounceTimerEnable = false;
+			debouncedButtonState = XGpio_DiscreteRead(&gpPB, 1);
+		}
+	}
+}
+
+void modifyTime_tick()
+{
+	static enum modifyTimer_states {modifyTimer_state_waitForTimeButton, modifyTimer_state_waitForUpDown, modifyTimer_state_AutoIncrement}
+		modifyTimer_state = modifyTimer_state_waitForTimeButton;
+	bool modifyTime = false;
+
+
+	// Wait for user to push a time button (left, center, or right)
+	int timeButton = debouncedButtonState & (PUSH_BUTTONS_LEFT | PUSH_BUTTONS_CENTER | PUSH_BUTTONS_RIGHT);
+	static int pushedTimeButton = 0;
+	if(modifyTimer_state == modifyTimer_state_waitForTimeButton && timeButton)
+	{
+		pushedTimeButton = timeButton; // Lock in the button. Don't allow user to push different time button
+		modifyTimer_state = modifyTimer_state_waitForUpDown;
+	}
+	else if(modifyTimer_state > modifyTimer_state_waitForTimeButton && !(timeButton & pushedTimeButton)) // Go backwards if released
+		modifyTimer_state = modifyTimer_state_waitForTimeButton;
+
+
+	// Wait for user to push up and down buttons
+	int upDown = debouncedButtonState & (PUSH_BUTTONS_UP | PUSH_BUTTONS_DOWN);
+	static int pushedUpDown = 0;
+	static int autoIncrementTimer = 0;
+	if(modifyTimer_state == modifyTimer_state_waitForUpDown && upDown)
+	{
+		pushedUpDown = upDown; // Lock in the button. Don't allow user to push different up down button
+		modifyTimer_state = modifyTimer_state_AutoIncrement;
+		modifyTime = true; // Modify time on initial press
+		autoIncrementTimer = 0;
+	}
+	else if(modifyTimer_state > modifyTimer_state_waitForUpDown && !(upDown & pushedUpDown)) // Go backwards if released
+		modifyTimer_state = modifyTimer_state_waitForUpDown;
+
+
+	// Auto increment time if held
+	if(modifyTimer_state == modifyTimer_state_AutoIncrement)
+	{
+		autoIncrementTimer++;
+		if(autoIncrementTimer > 100 && autoIncrementTimer % 50 == 0) // 1 sec
+		{
+			modifyTime = true;
+		}
+	}
+
+
+	// Modify time if in correct state.
+	if(modifyTime)
+	{
+		// Up or down?
+		int incVal;
+		if(pushedUpDown & PUSH_BUTTONS_UP)
+			incVal = 1;
+		else if(pushedUpDown & PUSH_BUTTONS_DOWN)
+			incVal = -1;
+
+		// Hours, minutes, or seconds?
+		if(pushedTimeButton & PUSH_BUTTONS_LEFT)
+			hours = (hours + incVal + 24) % 24;
+		else if(pushedTimeButton & PUSH_BUTTONS_CENTER)
+			minutes = (minutes + incVal + 60) % 60;
+		else if(pushedTimeButton & PUSH_BUTTONS_RIGHT)
+			seconds = (seconds + incVal + 60) % 60;
+		displayTime_display();
+	}
+}
+
+int displayTime_timer = 0;
+
+void displayTime_increment()
+{
+	seconds++;
+	if (seconds > 59) {
+		seconds = 0;
+		minutes++;
+		if (minutes > 59) {
+			minutes = 0;
+			hours++;
+			if (hours > 23) {
+				hours = 0;
+			}
+		}
+	}
+}
+
+void displayTime_display()
+{
+	xil_printf("%02d:%02d:%02d \r", hours, minutes, seconds);
+}
+
+void displayTime_tick()
+{
+	displayTime_timer++;
+	if (displayTime_timer == 100)
+	{
+		displayTime_timer = 0;
+		displayTime_increment();
+		displayTime_display();
+	}
+}
 
 // This is invoked in response to a timer interrupt.
 // It does 2 things: 1) debounce switches, and 2) advances the time.
-void timer_interrupt_handler()
+void timer_interrupt_handler() //10ms
 {
-	static int i = 0;
-	i++;
-	if(i == 1000)
-	{
-		//XPAR_PUSH_BUTTONS_5BITS_BASEADDR
-		i = 0;
-		print("+ 10 seconds\n\r");
-	}
-	if(buttonTimer > 0)
-		buttonTimer--;
+	debounceButtons_tick();
+	modifyTime_tick();
+	displayTime_tick();
 }
 
 // This is invoked each time there is a change in the button state (result of a push or a bounce).
 void pb_interrupt_handler()
 {
 	// Clear the GPIO interrupt.
-	XGpio_InterruptGlobalDisable(&gpPB);                // Turn off all PB interrupts for now.
+	XGpio_InterruptGlobalDisable(&gpPB);                    // Turn off all PB interrupts for now.
 	int currentButtonState = XGpio_DiscreteRead(&gpPB, 1);  // Get the current state of the buttons.
-
-	if(currentButtonState && buttonTimer == 0)
+	static int previousButtonState;
+	if (previousButtonState != currentButtonState)
 	{
-		buttonTimer = 20;
-		xil_printf("do something %d \n\r", currentButtonState);
+		debounceTimer = 0;
+		debounceTimerEnable = true;
+		previousButtonState = currentButtonState;
 	}
 
 	// You need to do something here.
@@ -64,12 +180,6 @@ void interrupt_handler_dispatcher(void* ptr)
 	}
 	// Check the push buttons.
 	if (intc_status & XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK)
-	{
-		XIntc_AckIntr(XPAR_INTC_0_BASEADDR, XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK);
-		pb_interrupt_handler();
-	}
-	// Check the switches
-	if (intc_status & XPAR_DIP_SWITCHES_8BITS_IP2INTC_IRPT_MASK)
 	{
 		XIntc_AckIntr(XPAR_INTC_0_BASEADDR, XPAR_PUSH_BUTTONS_5BITS_IP2INTC_IRPT_MASK);
 		pb_interrupt_handler();
