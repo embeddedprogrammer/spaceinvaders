c41350899c5bd349eb48fe2c214ce7db14375fa8
Jacob White <superman@CB400-05.WORKGROUP>
2015-11-12 17:30:04 -0700

    Half-done code for tri-state read and write.

--- ISE_PS2_Simulation/PS2.v
+++ ISE_PS2_Simulation/PS2.v
@@ -21,8 +21,7 @@
 module PS2
 (
 	IP_Interupt,
-	bitsToSend,
-	bitsReceived,
+	bitpipe,
 	Din,
 	Dout,
   // -- DO NOT EDIT BELOW THIS LINE ------------------
@@ -50,10 +49,10 @@ parameter C_SLV_DWIDTH                   = 32;
 // -- DO NOT EDIT ABOVE THIS LINE --------------------
 
 // -- ADD USER PORTS BELOW THIS LINE -----------------
-input                                     Din;
+inout                                     PS2_D;
+inout                                     PS2_CLK;
 output                                    IP_Interupt;
-output                                    bitsToSend;   // TODO: REMOVE
-output                                    bitsReceived; // TODO: REMOVE
+output                                    bitpipe;      // TODO: REMOVE
 output                                    Dout;         
 // -- ADD USER PORTS ABOVE THIS LINE -----------------
 
@@ -85,6 +84,34 @@ output                                    IP2Bus_Error;
 	wire                                      slv_read_ack;
 	wire                                      slv_write_ack;
 	integer                                   byte_index, bit_index;
+	reg                                       sendingData;
+	reg        [14]                           counter; //To pull clock line low
+
+	reg usingPS2_CLK;
+	reg usingPS2_D;
+	assign PS2_D = (usingPS2_D && !Dout) ? 0 : 'bz;
+	assign PS2_CLK = (usingPS2_CLK && !CLKout) ? 0 : 'bz;
+
+	reg last_PS2_CLK;
+
+	if !sendingData
+		
+	else if receivingData
+	else if Din
+		sendingData <= 1;
+	else if 
+	end
+
+localparam [2:0]
+	IDLE = 3'd0,
+	DRIVE_CLOCK_LOW = 3'd1,
+	SENDING_DATA    = 3'd2;
+	ACKNOWLEDGE     = 3'd3;
+	RECEIVING_DATA  = 3'd4;
+localparam [13:0]
+	MAXCOUNT = 14'd10_000; //At 100MHz this will give us 100 micro seconds.
+	
+reg [2:0] state;
 
   assign
     slv_reg_write_sel = Bus2IP_WrCE[1:0],
@@ -97,11 +124,88 @@ output                                    IP2Bus_Error;
 	begin
 		if(Bus2IP_Resetn == 1'b0)
 		begin
-			bitsToSend <= 11'b11111111111;
-			bitsReceived <= 11'b11111111111;
+			//reset
+			state = IDLE;
 		end
 		else
 		begin
+			if (state == IDLE)
+			begin
+				if(requestToSend) //Should we allow overriding the signal when the mouse is already talking to us?
+				begin
+					usingPS2_CLK <= 1;
+					usingPS2_D <= 1;
+					CLKout <= 0;
+					Dout <= 0;
+					counter <= 0;
+					state <= DRIVE_CLOCK_LOW;
+					requestToSend <= 0;
+				end
+				else if(Din)
+					state <= RECEIVING_DATA;
+			end
+			else if (state == DRIVE_CLOCK_LOW)
+			begin
+					if(counter < MAXCOUNT)
+						counter <= counter + 1;
+					else
+					begin
+						counter <= 0;
+						state <= SENDING_DATA;
+						last_PS2_CLK <= PS2_CLK;
+						usingPS2_CLK <= 0;
+						usingPS2_D <= 1;
+						Dout <= 0; //Start bit
+					end				
+			end
+			else if (state == SENDING_DATA)
+			begin
+				if(last_PS2_CLK && !PS2_CLK) //On falling edge
+				begin
+					counter++;
+					//Need some sort of way to select bit
+					// Idea 1: shift bits using registers?
+					// Idea 2: bit selector (counter, like below?)
+					
+					//if counter == 0 
+					//	Dout = 0; //Start bit
+					//else 
+					if(counter == 1) //Should this go outside of always block?
+						Dout <= D0;
+					else if(counter == 2)
+						Dout <= D1;
+						... //Fill in
+					else if(counter == 8)
+						Dout <= D7;
+					else if(counter == 8)
+						Dout <= parity;
+					else if(counter == 9)
+						usingPS2_D <= 0;
+						Dout <= 1; //stop bit
+						counter <= 0;
+					else if(counter == 10)
+						state <= ACKNOWLEDGE;
+					end
+				end
+				last_PS2_CLK <= PS2_CLK;
+			end
+			else if (state == ACKNOWLEDGE)
+				last_PS2_CLK <= PS2_CLK;
+				if(!last_PS2_CLK && PS2_CLK) //On rising edge
+				begin
+					//Store acknowledge bit in control register
+				
+				end
+			else if (state == RECEIVING_DATA)
+			begin
+				//counter to store bits in register, or shift bits.
+			end	
+		end //reset_n	
+	end //always	
+		
+				
+		// Old code below:
+		
 			case(slv_reg_write_sel)
 				2'b10: //reg 0 (read Din)
 				begin
@@ -121,6 +225,7 @@ output                                    IP2Bus_Error;
 					bitsReceived[9:0] <= bitsReceived[10:1]; // Shift bits to continue receiving data.
 					bitsReceived[10] <= Din;
 				end
+				
 			endcase
 		end
 	end
