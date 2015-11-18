40e1e82f5234b390e6c85a565492005be2d39ebb
Jacob White <superman@CB400-05.WORKGROUP>
2015-11-18 11:05:36 -0700

    Put receive and transmit submodules into final modules. Now just need to finish connecting the receive and transmit ports, and update the test bench (PS2_sim.v). After that we need to debug stuff. Note: SourceTree seems to have trouble changing username and email from the GUI. An easier method is to change it directly from the command line, using git bash. See https://help.github.com/articles/setting-your-username-in-git/

--- ISE_PS2_Simulation/PS2.v
+++ ISE_PS2_Simulation/PS2.v
@@ -20,10 +20,15 @@
 //////////////////////////////////////////////////////////////////////////////////
 module PS2
 (
-	IP_Interupt,
-	bitpipe,
-	Din,
-	Dout,
+	IP_Interrupt,
+	C_T,
+	C_I,
+	C_O,
+	D_T,
+	D_I,
+	D_O,
+	bitsReceived,
+	bitsToSend,
   // -- DO NOT EDIT BELOW THIS LINE ------------------
   // -- Bus protocol ports, do not add to or delete 
   Bus2IP_Clk,                     // Bus to IP clock
@@ -49,11 +54,14 @@ parameter C_SLV_DWIDTH                   = 32;
 // -- DO NOT EDIT ABOVE THIS LINE --------------------
 
 // -- ADD USER PORTS BELOW THIS LINE -----------------
-inout                                     PS2_D;
-inout                                     PS2_CLK;
-output                                    IP_Interupt;
-output                                    bitpipe;      // TODO: REMOVE
-output                                    Dout;         
+output                                    IP_Interrupt;
+output                                    C_T;
+input                                     C_I;
+output                                    C_O;
+output                                    D_T;
+input                                     D_I;
+output                                    D_O;
+output
 // -- ADD USER PORTS ABOVE THIS LINE -----------------
 
 // -- DO NOT EDIT BELOW THIS LINE --------------------
@@ -75,9 +83,10 @@ output                                    IP2Bus_Error;
 //----------------------------------------------------------------------------
 
   // Nets for user logic slave model s/w accessible register example
-	reg        [10:0]                         bitsToSend;
-	reg        [10:0]                         bitsReceived;
-	wire                                      IP_Interupt;
+	wire       [10:0]                         bitsReceived;
+	wire       [11:0]                         bitsToSend;
+	wire                                      IP_Interrupt;
+	
 	wire       [1 : 0]                        slv_reg_write_sel;
 	wire       [1 : 0]                        slv_reg_read_sel;
 	reg        [C_SLV_DWIDTH-1 : 0]           slv_ip2bus_data;
@@ -86,33 +95,29 @@ output                                    IP2Bus_Error;
 	integer                                   byte_index, bit_index;
 	reg                                       sendingData;
 	reg        [14]                           counter; //To pull clock line low
+	reg        [2:0]                          state;
+	wire                                      Load;
+	reg        [7:0]                          LoadVal;
 
-	reg usingPS2_CLK;
-	reg usingPS2_D;
-	assign PS2_D = (usingPS2_D && !Dout) ? 0 : 'bz;
-	assign PS2_CLK = (usingPS2_CLK && !CLKout) ? 0 : 'bz;
+	localparam [2:0]
+		IDLE_RECEIVE = 3'd0,
+		CLOCK_LOW    = 3'd1,
+		SEND         = 3'd2;
+		WAIT         = 3'd3;
+		ACK          = 3'd4;
 
-	reg last_PS2_CLK;
+	localparam [13:0]
+		MAXCOUNT = 14'd10_000; //At 100MHz this will give us 100 micro seconds.
 
-	if !sendingData
-		
-	else if receivingData
-	else if Din
-		sendingData <= 1;
-	else if 
-	end
-
-localparam [2:0]
-	IDLE = 3'd0,
-	DRIVE_CLOCK_LOW = 3'd1,
-	SENDING_DATA    = 3'd2;
-	ACKNOWLEDGE     = 3'd3;
-	RECEIVING_DATA  = 3'd4;
-localparam [13:0]
-	MAXCOUNT = 14'd10_000; //At 100MHz this will give us 100 micro seconds.
+	assign PS2_CLK = C_T;
+	assign Resetn = Bus2IP_Resetn;
+	assign Interrupt = IP_Interrupt;
+	assign Load = (state == IDLE) && (slv_reg_write_sel == 2'b10); //Load on transition from Idle to Clock Low
+	assign LoadVal = 
 	
-reg [2:0] state;
-
+	Transmitter transmitter1 (PS2_CLK, Resetn, Load, LoadVal, D_OUT, Done, bitsToSend);
+	Receiver    receiver1    (PS2_CLK, Resetn, Interrupt, ReadVal, D_IN, bitsReceived);
+	
   assign
     slv_reg_write_sel = Bus2IP_WrCE[1:0],
     slv_reg_read_sel  = Bus2IP_RdCE[1:0],
@@ -124,122 +129,42 @@ reg [2:0] state;
 	begin
 		if(Bus2IP_Resetn == 1'b0)
 		begin
-			//reset
-			state = IDLE;
+			state <= IDLE;
 		end
 		else
-		begin
-			if (state == IDLE)
-			begin
-				if(requestToSend) //Should we allow overriding the signal when the mouse is already talking to us?
-				begin
-					usingPS2_CLK <= 1;
-					usingPS2_D <= 1;
-					CLKout <= 0;
-					Dout <= 0;
-					counter <= 0;
-					state <= DRIVE_CLOCK_LOW;
-					requestToSend <= 0;
-				end
-				else if(Din)
-					state <= RECEIVING_DATA;
-			end
-			else if (state == DRIVE_CLOCK_LOW)
-			begin
-					if(counter < MAXCOUNT)
-						counter <= counter + 1;
-					else
-					begin
-						counter <= 0;
-						state <= SENDING_DATA;
-						last_PS2_CLK <= PS2_CLK;
-						usingPS2_CLK <= 0;
-						usingPS2_D <= 1;
-						Dout <= 0; //Start bit
-					end				
-			end
-			else if (state == SENDING_DATA)
-			begin
-				if(last_PS2_CLK && !PS2_CLK) //On falling edge
-				begin
-					counter++;
-					//Need some sort of way to select bit
-					// Idea 1: shift bits using registers?
-					// Idea 2: bit selector (counter, like below?)
-					
-					//if counter == 0 
-					//	Dout = 0; //Start bit
-					//else 
-					if(counter == 1) //Should this go outside of always block?
-						Dout <= D0;
-					else if(counter == 2)
-						Dout <= D1;
-						... //Fill in
-					else if(counter == 8)
-						Dout <= D7;
-					else if(counter == 8)
-						Dout <= parity;
-					else if(counter == 9)
-						usingPS2_D <= 0;
-						Dout <= 1; //stop bit
-						counter <= 0;
-					else if(counter == 10)
-						state <= ACKNOWLEDGE;
-					end
-				end
-				last_PS2_CLK <= PS2_CLK;
-			end
-			else if (state == ACKNOWLEDGE)
-				last_PS2_CLK <= PS2_CLK;
-				if(!last_PS2_CLK && PS2_CLK) //On rising edge
-				begin
-					//Store acknowledge bit in control register
-				
-				end
-			else if (state == RECEIVING_DATA)
-			begin
-				//counter to store bits in register, or shift bits.
-			end	
-		end //reset_n	
-	end //always	
-		
-				
-		// Old code below:
-		
-			case(slv_reg_write_sel)
-				2'b10: //reg 0 (read Din)
-				begin
-					bitsReceived <= Bus2IP_Data[11:0]; // TODO: Make this be a control bit.
-				end
-				2'b01: //reg 1 (write to Dout)
-				begin
-					bitsToSend[0] <= 0; // Start bit
-					bitsToSend[8:1] <= Bus2IP_Data[7:0]; // Data
-					bitsToSend[9] <= ~^Bus2IP_Data[7:0]; // Bitwise reduction XNOR for odd parity bit.
-					bitsToSend[10] <= 1; // Stop bit
-				end
+		begin	
+			case(state)
+				IDLE:
+					if(slv_reg_write_sel == 2'b10)
+						state <= CLOCK_LOW;
+				CLOCK_LOW:
+					if(counter == MAXCOUNT) //TODO: Make counter count
+						state <= SEND;
+				SEND:
+					if(Done == 0'b1)
+						state <= WAIT;
+				WAIT:
+					if(D_I == 0'b1)
+						state <= ACK;
+				ACK:
+					if(D_I == 1'b1)
+						state <= IDLE;
 				default:
-				begin
-					bitsToSend[9:0] <= bitsToSend[10:1]; // Shift bits to continue sending data.
-					bitsToSend[10] <= 1;
-					bitsReceived[9:0] <= bitsReceived[10:1]; // Shift bits to continue receiving data.
-					bitsReceived[10] <= Din;
-				end
-				
+					state <= IDLE;
 			endcase
 		end
 	end
-
+	
   // Slave register read mux
   always @(slv_reg_read_sel or bitsReceived or bitsToSend)
     begin
       case(slv_reg_read_sel)
         2'b10:
 				begin
-					slv_ip2bus_data <= bitsReceived[8:1];
+					slv_ip2bus_data <= bitsToSend;
 				end
         2'b01:
-					slv_ip2bus_data <= bitsToSend[8:1];
+					slv_ip2bus_data <= ReadVal;
         default:
 					slv_ip2bus_data <= 0;
       endcase
@@ -251,5 +176,5 @@ reg [2:0] state;
   assign IP2Bus_RdAck = slv_read_ack;
   assign IP2Bus_Error = 0;
   assign Dout = bitsToSend[0];
-  assign IP_Interupt = !bitsReceived[0];
+  assign IP_Interrupt = !bitsReceived[0];
 endmodule
